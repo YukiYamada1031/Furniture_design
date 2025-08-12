606064f99908e259584906dea21fec828a806d40
“YukiYamada1031” <office.deborah.0217@gmail.com>
2025-08-12 09:02:08 +0900

    practice jquery

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ $(function(){
   });
 });
 */
+
+
+$(function(){
+  $('.toggle-btn').on('click', function(){
+    if($('#header').hasClass('open')){
+
+      $('#header').removeClass('open');
+
+    } else {
+
+      $('#header').addClass('open');
+    }
+  });
+
+  $('.mask').on('click' , function(){
+    $('#header').removeClass('open');
+  });
+});
