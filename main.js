f89abd2523933cff7910d21238064a3f95222258
“YukiYamada1031” <office.deborah.0217@gmail.com>
2025-08-13 22:54:17 +0900

    practice js

--- main.js
+++ main.js
@@ -34,3 +34,20 @@ $(function(){
     $('#header').removeClass('open');
   });
 });
+
+$(function(){
+  $('.toggle-btn').on('click', function(){
+    if($('#header').hasClass('open')){
+
+      $('#header').removeClass('open');
+
+    } else {
+
+      $('#header').addClass('open');
+    }
+  });
+  
+  $('.mask').on('click', function(){
+    $('#header').removeClass('open');
+  });
+});
