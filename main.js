f64a8e1b2a4dedd8f3d02ec81bc0dec66f4a3f15
“YukiYamada1031” <office.deborah.0217@gmail.com>
2025-08-17 22:40:24 +0900

    make index.html

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 $(function () {
   $('toggle-btn').on('click', function () {
     if ($('#header').hasClass('open')) {
@@ -15,3 +16,5 @@ $(function () {
     $('#header').removeClass('open');
   });
 });
+
+*/
